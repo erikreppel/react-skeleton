376547d2dbe540682cd0ff6c304833761816c6d3
Erik Reppel <erikreppel@gmail.com>
2016-04-09 14:15:54 -0700

    cleaned a few things up, still very much a work in progress

--- components/Counter/Counter.jsx
+++ components/Counter/Counter.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Counter extends React.Component {
   render() {
     return (
-        <h1>Count</h1>
+        <h1>Counter</h1>
     )
   }
 }
--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Router, Route, IndexRoute } from 'react-router'
+`use strict`;
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, IndexRoute } from 'react-router';
 
-import HelloWorld from './components/HelloWorld/HelloWorld.jsx'
-import Counter from './components/Counter/Counter.jsx'
+import HelloWorld from './components/HelloWorld/HelloWorld.jsx';
+import Counter from './components/Counter/Counter.jsx';
 
 
 render((
     <Router>
         <Route path="/" component={HelloWorld}> 
             <Route path="counter" component={Counter}/>
-        </Route>
-        <Route path="/count" component={Counter}/>
+        </Route>        
     </Router>
     ), document.getElementById('content'))
